09f1f6fb620498850e07e55c0b21ac58f71a63a7
YanZhangNZ <crazyanz888@gmail.com>
2019-08-28 23:16:46 +1200

    test Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './header/index'
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
-      app
+      <Header />
     </div>
   );
 }
