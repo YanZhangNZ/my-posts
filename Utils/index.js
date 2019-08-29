6549401e43ab4b867646f52b88c66385f3f3983b
YanZhangNZ <crazyanz888@gmail.com>
2019-08-29 21:00:39 +1200

    test headline component with props and with no props

--- Utils/index.js
+++ Utils/index.js
@@ -1,6 +1,6 @@
 import checkPropTypes from 'check-prop-types';
 
-export const findByTestAtrr = (component, attr) => {
+export const findByTestAttr = (component, attr) => {
     const wrapper = component.find(`[data-test='${attr}']`);
     return wrapper;
 };
--- src/components/header/spec.js
+++ src/components/header/spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { findByTestAtrr } from './../../../Utils';
+import { findByTestAttr } from './../../../Utils';
 import Header from './index';
 
 const setUp = (props={}) => {
@@ -16,12 +16,12 @@ describe('Header Component', () => {
     });
 
     it('Should render without errors', () => {
-        const wrapper = findByTestAtrr(component, 'headerComponent');
+        const wrapper = findByTestAttr(component, 'headerComponent');
         expect(wrapper.length).toBe(1);
     });
 
     it('Should render a logo', () => {
-        const logo = findByTestAtrr(component, 'logoIMG');
+        const logo = findByTestAttr(component, 'logoIMG');
         expect(logo.length).toBe(1);
     });
 
